bf94d49b20ea8aec56e2053c63153efc0affc02f
Tomas Calderaro <tomasjcalderaro@gmail.com>
2025-07-03 14:19:01 -0300

    Problema buscador resuelto

--- src/BuscadorPeliculas.jsx
+++ src/BuscadorPeliculas.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 export const BuscadorPeliculas = () => {
     
     const urlBase = 'https://api.themoviedb.org/3/search/movie'
-    const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+    const API_KEY = '1f8a7dd3e92e11985676bcff8a37fec7'
     const [busqueda, setBusqueda] = useState('')
     const [peliculas, setPeliculas] = useState([])
     
@@ -48,13 +48,11 @@ export const BuscadorPeliculas = () => {
         </form>
         {/* Aquí puedes renderizar las películas */}
         <div className="movie-list">
-            {peliculas.map(pelicula => (
+            {peliculas.map( (pelicula) => (
                 <div key={pelicula.id} className="movie-card">
+                    <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} alt={pelicula.title} />
                     <h2>{pelicula.title}</h2>
                     <p>{pelicula.overview}</p>
-                    {pelicula.poster_path && (
-                        <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} alt={pelicula.title} />
-                    )}
                 </div>
             ))}
         </div>
